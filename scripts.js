09be6efb376e605f0a4bc1265b6c2b187e7db360
Nadia <nadikorableva@gmail.com>
2022-12-15 12:45:34 -0500

    css

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
 const sidebar = document.getElementById("sidebar");
+const step1 = document.getElementById("step1");
+const step2 = document.getElementById("step2");
+const step3 = document.getElementById("step3");
+const step4 = document.getElementById("step4");
+const step5= document.getElementById("step5");
+const subscription = document.getElementById("subscription");
 
+
+step2.style.display="none";
+step3.style.display="none";
+step4.style.display="none";
+step5.style.display="none";
+subscription.style.display="none";
